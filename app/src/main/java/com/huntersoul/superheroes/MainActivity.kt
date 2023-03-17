0f12b36b9df33b12b7c90ad53019a0bbb1f1756c
EstebanTM <s18120211@alumnos.itsur.edu.mz>
2023-03-17 12:03:46 -0600

    Agregar recursos y crear funcion principal

--- app/src/main/java/com/huntersoul/superheroes/MainActivity.kt
+++ app/src/main/java/com/huntersoul/superheroes/MainActivity.kt
@@ -3,20 +3,26 @@ package com.huntersoul.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.huntersoul.superheroes.ui.theme.SuperHeroesTheme
+import com.huntersoul.superheroes.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SuperHeroesTheme {
+            SuperheroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -25,6 +31,19 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
+        }
+    }
+}
+@Composable
+fun HeroItem(modifier: Modifier = Modifier){
+    Card() {
+        Row() {
+            Column() {
+                Text(text = stringResource(id = R.string.hero1))
+                Text(text = stringResource(id = R.string.description1))
+            }
+            Image(painter = painterResource(id = R.drawable.android_superhero1), 
+                contentDescription = null)
         }
     }
 }
